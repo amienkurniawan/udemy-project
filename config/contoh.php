40a96c82b82dac71b159cc8ed53d68cc313b15ed
Amien kurniawan <offdevamienk@gmail.com>
2023-02-28 22:37:25 +0700

    config

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  "author" => [
+    "first" => "Amien",
+    "last" => "Kurniawan"
+  ],
+  "email" => "offdevamienk@gmail.com",
+  "website" => "akstudios.com"
+];
---
+++ tests/Feature/ConfigurationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ConfigurationTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_running_config()
+    {
+        $first_name = config("contoh.author.first");
+        $last_name = config("contoh.author.last");
+        $email = config("contoh.email");
+
+
+
+        self::assertEquals('Amien', $first_name);
+    }
+}
